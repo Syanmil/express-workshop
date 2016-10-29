bc179fbdc713d5f76a292e6370c4379fc337b1a1
Your Name <name@example.com>
2016-10-29 09:40:05 +0700

    express initial setup

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Express dependencies
+const express = require('express');
+const body-parser = require('body-parser');
+const cors = require('cors');
+
+//Initiate Express
+const app = express();
+const router = app.Router();
