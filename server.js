9dd6bcab4ca6d29d096fe4b1f5db28575b304b52
Your Name <name@example.com>
2016-10-29 09:58:45 +0700

    npm run for start and dev

--- server.js
+++ server.js
@@ -4,18 +4,19 @@
 
 //Express dependencies
 const express = require('express');
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 //Initiate Express
 const app = express();
-const router = app.Router();
+const router = express.Router();
 
 //APP Configuration
 
 //req.body
 app.use(bodyParser.urlencoded({extended:true})); //ditambah urlencoded agar dapat input dari form
 app.use(bodyParser.json())
-app.user(cors())
+app.use(cors())
 
-const data = require('./data.json')
+const books = require('./data.js');
+console.log(books);
