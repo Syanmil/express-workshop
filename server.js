011a3bd46bdefeb93debb3cb28589a50af0a52a8
Your Name <name@example.com>
2016-10-29 09:44:58 +0700

    setting express body parser and cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//NODE MODULES
+
 //Express dependencies
 const express = require('express');
 const body-parser = require('body-parser');
@@ -8,3 +10,12 @@ const cors = require('cors');
 //Initiate Express
 const app = express();
 const router = app.Router();
+
+//APP Configuration
+
+//req.body
+app.use(bodyParser.urlencoded({extended:true})); //ditambah urlencoded agar dapat input dari form
+app.use(bodyParser.json())
+app.user(cors())
+
+const data = require('./data.json')
